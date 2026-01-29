623a34a13cab904ad7752c217916a4d73868306b
Utkarsh Yadav <utkarshprogrammer2005@gmail.com>
2026-01-30 00:05:04 +0530

    learn data types

---
+++ 02_syntax.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  // This is a single line comment.
+
+  /*
+  This is a
+  multiline comment.
+  */
+
+  cout << "IndieDev\n";
+  cout << 3 << "\n";
+  cout << 3 + 3 << "\n";
+
+  return 0;
+}
---
+++ 04_user_input.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int a, b;
+  cout << "Enter the value of A: ";
+  cin >> a;
+
+  cout << "Enter the value of B: ";
+  cin >> b;
+
+  cout << a << " + " << b << " = " << a + b;
+
+  return 0;
+}
